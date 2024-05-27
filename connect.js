5f3b0907155c1af7c1a90c113fc7611c853ed7b2
Gabriel Gutierrez Quispe <111787735+GabrielGutierrezQuispe@users.noreply.github.com>
2024-05-27 00:26:40 -0500

    MMMMM

--- connect.js
+++ connect.js
@@ -1,9 +1,7 @@
 var express = require("express");
-var mysql = require("mysql");
 var http = require("http");
 var compression = require("compression");
 require('dotenv').config();
-const {v4: uuidv4} = require('uuid');
 var cors = require("cors");
 
 var app = express();
@@ -11,42 +9,10 @@ app.use(express.json());
 app.use(cors());
 app.use(compression());
 
-var conexion = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "admin",
-  database: "conexion",
-});
-
-conexion.connect(function (error) {
-  if (error) {
-    throw error;
-  } else {
-    console.log("Conexión exitosa");
-  }
-});
-
 const puerto = process.env.PUERTO || 3000;
 
 app.listen(puerto, function () {
   console.log("Servidor funcionando en puerto: " + puerto);
-});
-
-app.post("/api/contac", (req, res) => {
-  let data = {
-      NAAMES: req.body.NAAMES,
-      SUBJECTS: req.body.SUBJECTS,
-      EMAIL: req.body.EMAIL,
-      DESCRIPTION: req.body.DESCRIPTION
-  };
-  let sql = "INSERT INTO contac SET ?";
-  conexion.query(sql, data, function (error, results) {
-      if (error) {
-          throw error;
-      } else {
-          console.log(data);
-      }
-  });
 });
 
 // Servidor HTTP
